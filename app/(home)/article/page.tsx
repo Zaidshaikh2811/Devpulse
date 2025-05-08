9e053e27e28f3ccef8fcc7a3f42631cae5a50880
Mohammad Zaid Shaikh <zaidshaikhhulk@gmail.com>
2025-05-08 22:56:45 +0530

    refactor: improve type safety in Page component; enhance readability of article retrieval logic

--- app/(home)/article/page.tsx
+++ app/(home)/article/page.tsx
@@ -3,17 +3,23 @@ import { getAllArticles } from '@/actions/get-article';
 import AllArticle from '@/components/UI/AllArticle'
 import React from 'react'
 
-const Page = async ({ searchParams }: { searchParams: { page?: string; search?: string } }) => {
-    const page = Number(searchParams.page || 1);
-    const search = searchParams.search || '';
+
+const Page = async ({ searchParams }: any) => {
+    const page = Number(searchParams?.page || 1);
+    const search = searchParams?.search || '';
+
     const { articles, totalCount } = await getAllArticles({ page, search });
 
-
     return (
-        <div className='container'>
-            <AllArticle articles={articles} totalCount={totalCount} currentPage={page} searchQuery={search} />
+        <div className="container">
+            <AllArticle
+                articles={articles}
+                totalCount={totalCount}
+                currentPage={page}
+                searchQuery={search}
+            />
         </div>
     );
-}
+};
 
 export default Page;
